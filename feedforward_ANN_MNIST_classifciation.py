67f4682df43896ebf5074e1fe95a7c0239c31336
mdjoh <marchiano.oh@concordia.ca>
2022-10-23 20:48:28 -0400

    added model predictions and updated viz functions

--- feedforward_ANN_MNIST_classifciation.py
+++ feedforward_ANN_MNIST_classifciation.py
@@ -49,24 +49,23 @@ def split_data(cleaned_X, cleaned_Y):
 X_val, X_test, X_train, Y_val, Y_test, Y_train = split_data(cleaned_X, cleaned_Y)
 
 # Visualize data
-def visualize_data(X_train):
-    X_train_sample = X_train.to_numpy()[:10,]
+def visualize_data(X_train, n_samples):
+    X_train_sample = X_train.to_numpy()[:n_samples]
 
-    Y_train_sample = Y_train[:10,]
+    Y_train_sample = Y_train[:n_samples]
 
     plt.figure(figsize=(10,10))
 
     for i in range(len(X_train_sample)):
-      plt.subplot(2, 5, i+1)
+      plt.subplot(2, n_samples/2, i+1)
       plt.xticks([])
       plt.yticks([])
       plt.imshow(X_train_sample[i].reshape(28,28), cmap=plt.cm.binary)
       plt.xlabel(f"Digit: {Y_train[i].argmax()}")
 
     plt.show()
-    print(X_train_sample)
 
-visualize_data(X_train)
+visualize_data(X_train, 10)
 
 # Build model with 2 hidden layers with ReLu activation function and 20% dropout rate
 # softmax activation function in the final layer
@@ -105,3 +104,25 @@ def eval_model(model, X_test, Y_test):
     return test_loss, test_accuracy
 
 test_loss, test_accuracy = eval_model(model, X_test, Y_test)
+
+# Model prediction
+predictions = model.predict(X_test)
+
+# Visualize predictions
+def visualize_predictions(X_test, predictions, n_samples):
+    X_test_sample = X_test.to_numpy()[:n_samples]
+
+    Y_test_sample = Y_test[:n_samples]
+
+    plt.figure(figsize=(10,10))
+
+    for i in range(len(X_test_sample)):
+      plt.subplot(2, n_samples/2, i+1)
+      plt.xticks([])
+      plt.yticks([])
+      plt.imshow(X_test_sample[i].reshape(28,28), cmap=plt.cm.binary)
+      plt.xlabel(f"Predicted Digit: {predictions[i].argmax()}\nActual Digit: {Y_test_sample[i].argmax()}")
+
+    plt.show()
+
+visualize_predictions(X_test, predictions, 10)
